e709cd2a7bc04addfdc62e1a56c13d7ec7e69aa7
alihaider1998 <67502924+alihaider1998@users.noreply.github.com>
2022-11-22 01:41:34 +0100

    Update index.js

--- pages/form/index.js
+++ pages/form/index.js
@@ -7,7 +7,7 @@ function index() {
   <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Help Us With Feedback</h2>
       <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a sugesstion? Want to contribute towards models? Want to Report a malfunction? Let us know.</p>
-      <form action="https://formsubmit.co/fe0c7486e2e2516c897f6e0326e2bbde" method="POST" className="space-y-8">
+      <form action="https://formsubmit.co/4f04a10a5feaa25d63a6383190d342e1" method="POST" className="space-y-8">
           <div>
               <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
               <input type="email" name='Email' id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@deepsynthbody.com" required />
