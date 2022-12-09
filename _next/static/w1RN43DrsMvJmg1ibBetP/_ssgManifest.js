8a69a409c6e598d16da14091633ca0c83c5c1676
alihaider1998 <67502924+alihaider1998@users.noreply.github.com>
2022-12-09 01:31:20 +0000

    Deploying to gh-pages from @ simulamet-host/deepsynthbody@2e4bae82c2ebe648307933686e71baed17200e36 🚀

--- _next/static/w1RN43DrsMvJmg1ibBetP/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002F[subcategory]","\u002Fnews","\u002Fsubsubcategory\u002F3rdSubcategory\u002F[subsubsubcategory]","\u002Fsubsubcategory\u002F3rdSubcategory\u002Fdetails\u002F[detail]","\u002Fsubsubcategory\u002F[subsubcategory]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
