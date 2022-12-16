e5cbaf8ee4ac17ef7cb728c4048a6015548f01e0
Raja Ali Haider <rajaalihaider@Rajas-MacBook-Pro.local>
2022-12-16 23:46:14 +0100

    news sidebar n navbar active classes

--- components/navbar.js
+++ components/navbar.js
@@ -87,7 +87,7 @@ const NavBar = () => {
         <div className="hidden lg:block">{navList}</div>
         <IconButton
           variant="text"
-          className="float-right mr-3 h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          className="float-right mr-3 h-10 w-10 mb-2 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
           ripple={false}
           onClick={() => setOpenNav(!openNav)}
         >
@@ -95,7 +95,7 @@ const NavBar = () => {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              className="h-6 w-6"
+              className="h-7 w-7"
               viewBox="0 0 24 24"
               stroke="currentColor"
               strokeWidth={2}
@@ -109,7 +109,7 @@ const NavBar = () => {
           ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
+              className="h-7 w-7"
               fill="none"
               stroke="currentColor"
               strokeWidth={2}
