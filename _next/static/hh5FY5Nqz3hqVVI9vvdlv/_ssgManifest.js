7c063bc281fcf892fe0becc615f60d89cddb37e0
gizem28 <gizem28@users.noreply.github.com>
2023-01-04 09:50:15 +0000

    Deploying to gh-pages from @ simulamet-host/deepsynthbody@8313c53ad96f388656f5a7818d5bca38381b67ed 🚀

---
+++ _next/static/hh5FY5Nqz3hqVVI9vvdlv/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[subcategory]","\u002Fabout","\u002Fdocs","\u002Fnews\u002F[detail]","\u002Fnews\u002Fnews","\u002Fsubsubcategory\u002F3rdSubcategory\u002F[subsubsubcategory]","\u002Fsubsubcategory\u002F3rdSubcategory\u002Fdetails\u002F[detail]","\u002Fsubsubcategory\u002F[subsubcategory]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
